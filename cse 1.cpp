570c39ef0554389829cc85b7351916043bc93460
swasthikagarapati07 <swasthikagarapati@gmail.com>
2025-11-26 22:34:04 +0530

    Add files via upload

---
+++ cse 1.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	printf("i am swasthika\n");
+	printf("from nuzvid\n");
+	printf("my mother name is swarupa rani\n");
+	printf("my father name is rajashekar");
+return 0;
+}
