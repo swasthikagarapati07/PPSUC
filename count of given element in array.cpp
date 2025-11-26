212fe31deaf7ed67c9f8507130c35719af14c700
swasthikagarapati07 <swasthikagarapati@gmail.com>
2025-11-26 23:13:44 +0530

    Add files via upload

---
+++ count of given element in array.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int N, z;
+    scanf("%d", &N);
+
+    int arr[N];
+    for(int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &z);  // element to search
+
+    int count = 0;
+    for(int i = 0; i < N; i++) {
+        if(arr[i] == z) {
+            count++;
+        }
+    }
+
+    printf("%d", count);
+    return 0;
+}
+
