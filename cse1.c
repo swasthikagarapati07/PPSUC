04e757a1c14d7a2e8f30853128be3a142449ebbd
swasthikagarapati07 <swasthikagarapati@gmail.com>
2025-11-26 21:40:50 +0530

    Update cse1.c

--- cse1.c
+++ cse1.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 int main()
 {
-	printf("i am lakshmi\n");
-	printf("from veeravasaram\n");
-	printf("my mother name is bhavani\n");
-	printf("my father name is subbu");
+	printf("i am swasthika\n");
+	printf("from nuzvid\n");
+	printf("my mother name is swarupa rani\n");
+	printf("my father name is rajashekar");
 return 0;
 }
 
 	
 	
+
 
