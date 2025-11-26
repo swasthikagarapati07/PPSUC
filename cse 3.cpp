7d75ae18cf5ee1d00f75dbd0ecd10b3727a00f1b
swasthikagarapati07 <swasthikagarapati@gmail.com>
2025-11-26 22:31:04 +0530

    Add files via upload

---
+++ cse 3.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	printf("i am swasthika\n");
+	printf("i am from nuzvid\n");
+	printf("my father name is rajashekar\n");
+	printf("my mother name is swarupa rani\n");
+	printf("i am studying 1st year b.tech in aditya university\n");
+	printf("i want to become a software engineer");
+return 0;
+}
