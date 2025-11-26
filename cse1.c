5a0527b7ea0deae50ff70fb75e6e036611473e09
swasthikagarapati07 <swasthikagarapati@gmail.com>
2025-11-26 22:31:56 +0530

    Add files via upload

---
+++ cse1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	printf("i am lakshmi\n");
+	printf("from veeravasaram\n");
+	printf("my mother name is bhavani\n");
+	printf("my father name is subbu");
+return 0;
+}
+
+	
+	
+
