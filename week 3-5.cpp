ba7e9e6a733d9f11f4b8afc0c4443d64955c8230
swasthikagarapati07 <swasthikagarapati@gmail.com>
2025-11-26 22:20:40 +0530

    Add files via upload

---
+++ week 3-5.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int h,b;
+	float area;
+	printf("enter h,b values");
+	scanf("%d%d",&h,&b);
+	area=0.5*h*b;
+	printf("area of triangle is:%f",area);
+	return 0;
+}
